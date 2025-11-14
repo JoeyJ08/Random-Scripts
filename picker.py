b6b4941823cd56afe82eaca37210c5ac7f7b8eea
JoeyJ08 <j2500jj@gmail.com>
2025-11-14 20:55:59 +0000

    :(

--- picker.py
+++ picker.py
@@ -15,35 +15,40 @@ def put_in_new_item():
         main_list.append(new_word)
     elif temp_input == 2:
         pass
+    os.system('clear')
 
 def show_current_list():
     os.system('clear')
     for item in main_list:
-        print('item')
-        temp_input = input('this is the current list\n1. back')
+        print(item)
+        temp_input = input('this is the current list\n1. back\n')
         if temp_input == '1':
             pass
+    os.system('clear')
 
 def pull_random_item():
     os.system('clear')
 
 def empty_list():
-    os.system('clear')1
+    os.system('clear')
 
 #code
+os.system('clear')
 while user_is_quit == False:
-    os.system('clear')
     print('please choose a option \n1. put a new item into the list \n2. see current list \n3. pull out a random item from the list \n4. empty out list\n5. quit')
     temp_input = input('')
 
     if temp_input == '1':
         put_in_new_item()
     elif temp_input == '2':
-        pass
+        if len(main_list) == 0:
+            show_current_list()
+        else:
+            print('the list it currently empty')
     elif temp_input == '3':
-        pass
+        pull_random_item()
     elif temp_input == '4':
-        pass
+        empty_list()
     elif temp_input == '5':
-        print('thank for using')
+        print('thank you for using')
         user_is_quit = True
